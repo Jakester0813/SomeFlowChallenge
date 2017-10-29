e58b05d99019fe4b29547424c2b628dd428d8897
Jake <jakester0812@gmail.com>
2017-10-28 17:44:15 -0700

    Allowed the user to go back and forth between Analytics and Chat activities, and allowed SplashActivity to not have history so that the user could not go back to SplashActivity

--- app/src/main/java/com/jakester/someflowchallenge/activities/AnalyticsActivity.java
+++ app/src/main/java/com/jakester/someflowchallenge/activities/AnalyticsActivity.java
@@ -1,15 +1,39 @@
 package com.jakester.someflowchallenge.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jakester.someflowchallenge.R;
 
 public class AnalyticsActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Analytics");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.chat_menu) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_analytics, menu);
+        return true;
     }
 }
